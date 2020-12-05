3469f323ea89cc51317291f32dea778b8b1dccc1
swl <swl@163.com>
2020-12-05 23:54:17 +0800

    更新：添加注释

--- hello.go
+++ hello.go
@@ -1,7 +1,7 @@
-package main
+package main  // 指明main包
 
-import "fmt"
+import "fmt"  // 导入fmt包
 
-func main() {
-	fmt.Println("hello world")
+func main() {	// 定义main函数
+	fmt.Println("hello world")  // 输出 hello world 到控制台
 }
